9f31e19ffa29b06c85e9a497714589d8e7371c92
Kelley Jenkins <ckjenkins3@gmail.com>
2018-04-05 15:25:55 -0600

    list json returning but working on date formatting issue

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,7 @@
 class List < ApplicationRecord
+
   has_many :list_ingredients
   has_many :ingredients, {through: :list_ingredients}, dependent: :destroy
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :ingredients
+      get "/lists", to: "lists#index"
     end
   end
 
