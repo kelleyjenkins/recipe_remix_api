538c2f63a1880000d4d86e13029e74e789d19529
Kelley Jenkins <ckjenkins3@gmail.com>
2018-04-05 14:28:50 -0600

    creates seed file

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
-  
+
   has_many :list_ingredients
-  has_many :lists, {through: :list_ingredients}
+  has_many :lists, {through: :list_ingredients}, dependent: :destroy
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.destroy_all
+List.destroy_all
+
+Ingredients = [
+  ["Apples"],
+  ["Flour"],
+  ["Sugar"],
+  ["Salt"],
+]
+
+Lists = [
+  ["List1", Date.new(2018, 3, 4)],
+  ["List2", Date.new(2018, 4, 15)]
+]
+
+Ingredients.each do |ingredient|
+  new_ingredient = Ingredient.create!(name: ingredient.first)
+  puts "Created new ingredient"
+end
+
+Lists.each do |list|
+  new_list = List.create!(name: list.first, date: list.last)
+  puts "Created #{new_list.name}"
+end
+
+List.all.each do |list|
+  3.times do
+    random_id = rand(Ingredient.first.id..Ingredient.last.id)
+    list.ingredients << Ingredient.find(random_id)
+  end
+end
+#
+# ListIngredients.each do |item|
+#   new_item = ListIngredient.create!(list: item.first, ingredient: item.last)
+#   puts "Created list ingredient"
+# end
