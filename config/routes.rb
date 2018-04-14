d10b729e9ad075c06b785127ab93e7f1dd8776a5
Kelley Jenkins <ckjenkins3@gmail.com>
2018-04-14 14:46:13 -0600

    created api route for user lists

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :ingredients
-      resources :users
-      namespace :users do
+      resources :users do
         resources :lists
       end
       get "/lists", to: "lists#index"
