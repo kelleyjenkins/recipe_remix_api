cd56c6a90c4069d1cb487c312409b1cb28d608fd
Kelley Jenkins <ckjenkins3@gmail.com>
2018-08-20 13:07:41 -0400

    requires user_id for lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
   has_many :list_ingredients, dependent: :destroy
   has_many :ingredients, {through: :list_ingredients}
 
